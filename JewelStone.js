d1afc9818b20a8adbf245bccd80bf0a821342c05
zersoon <aixiaozhou1324@126.com>
2018-12-19 20:36:49 +0800

    write some ways on it

--- JewelStone.js
+++ JewelStone.js
@@ -1,4 +1,4 @@
-{
+{   
 	var numJewelsInStone = function (J, S) {
 		let count = 0;
 		for(let i = 0; i < S.length; i++) {
@@ -7,5 +7,36 @@
 			}
 		}
 		return count;
+	}
+
+}
+
+{
+	const numJewelsInStone = (J, S) => S.split("").filter(char => J.indexOf(char) != -1).length;
+}
+
+{
+	var numJewelsInStone = function (J, S) {
+		return S.replace(new RegExp(`[^${J}]`, "g"), "").length;
+	}
+}
+
+{
+	var numJewelsInStone = function (J, S) {
+		const map = new Map();
+		let cnt = 0;
+		for(let i = 0; i < J.length; i++) {
+			map.set(J.charAt(i), 1)
+		}
+
+		for(let i = 0; i < S.length; i++) {
+			map.has(S.charAt(i)) && cnt++
+		}
+		return cnt;
+	}
+}
+{
+	function numJewelsInStone = function (J, S) {
+		return S.split("").filter(s => J.includes(s)).length;
 	}
 }
